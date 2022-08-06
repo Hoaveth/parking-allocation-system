15380ee60919629b1112f44a4a58899185f32438
Hoaveth <hoaveth.gaming@gmail.com>
2022-08-06 09:16:25 +0800

    Removed unused file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ParkingForm from "./pages/ParkingForm";
 import ParkingLot from "./pages/ParkingLot";
